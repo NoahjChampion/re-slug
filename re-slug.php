a3e637216e79d4d0112e7010dc64f5439f7cf7cc
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-03-29 12:50:49 -0600

    Update re-slug.php

--- re-slug.php
+++ re-slug.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Re-slug Updated
 Plugin URI: https://github.com/lukecav/re-slug
-Description: Regenerate the post permalink after changing the title
+Description: Regenerate the post permalink after changing the title.
 Author: Luke Cavanagh
 Author URI: https://profiles.wordpress.org/lukecavanagh
 Version: 1.1
